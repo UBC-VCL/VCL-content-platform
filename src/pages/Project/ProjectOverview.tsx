61f796b60ffdf9a3818f91abf59458b262ec891f
svolterra <sarahkalmm1902@gmail.com>
2022-06-19 13:33:07 -0700

    Design front-end portion of Project Overview page.

--- src/pages/Project/ProjectOverview.tsx
+++ src/pages/Project/ProjectOverview.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useAppSelector } from '@redux/hooks';
 import { selectProjects } from '@redux/slices/ProjectRedux';
 import GenericLink from '@components/generics/Link'
-import { ROUTES } from '@statics';
+import { ROUTES, TEXT } from '@statics';
 import "./ProjectOverview.css";
 
 const ProjectOverview = () => {
@@ -14,12 +14,13 @@ const ProjectOverview = () => {
                 <h2 className='project-overview-title'>All Projects</h2>
                 <hr/>
             </div>
+            {/* TODO: refactor to fetch data from backend, currently hard-coded */}
             <div className='button-container'>
-                <button className="project-button">Correlation</button>
-                <button className="project-button">IDEO</button>
-                <button className="project-button">Image Transitions</button>
-                <button className="project-button">NOVA</button>
-                <button className="project-button">Perceptual Modes</button>
+                <button className="project-button">{TEXT.PROJECT_NAMES.CORRELATION}</button>
+                <button className="project-button">{TEXT.PROJECT_NAMES.IDEO}</button>
+                <button className="project-button">{TEXT.PROJECT_NAMES.IMG_TRANSITIONS}</button>
+                <button className="project-button">{TEXT.PROJECT_NAMES.NOVA}</button>
+                <button className="project-button">{TEXT.PROJECT_NAMES.PERCEPTUAL_MODES}</button>
             <div>
                 {projects.map((project, i) => {
                     return (

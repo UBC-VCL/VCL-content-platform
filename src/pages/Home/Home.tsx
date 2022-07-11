01cf658802102f627a0c8b92dfdae19c6eae547d
Kat <katziv@students.cs.ubc.ca>
2022-07-11 19:29:58 +0200

    created component for landing page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,44 +1,13 @@
 import React from "react";
 import './Home.css';
-import { TEXT } from '@statics';
-import Card from "@components/Card";
-import "./Home.css"
-
+import LandingPage from '../../components/LandingPage/LandingPage';
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = (props) => {
   return (
     <div className="Home">
-      <div className="landing_page_top_section">
-        <div className="t_row">
-            <div className="landing_page_title">
-              <p>{TEXT.LANDING_PAGE.TITLE}</p>
-            </div>
-          </div>
-          <div className="t_row">
-            <div className="img_placeholder"></div>
-          <div className="t_row">
-              <div className="landing_page_description">
-                <p>{TEXT.LANDING_PAGE.DESCRIPTION}</p>
-              </div>
-          </div>
-          <div className="btn_ubc_home">
-            <a href="https://www.ubc.ca" style={{display: "flex", justifyContent: "center", alignItems: "center", color: "#ffffff"}}>
-              <p>{TEXT.LANDING_PAGE.UBC_PAGE_BUTTON}</p>
-            </a>
-          </div>
-        </div>
-        </div>
-        {/*
-        <div>HI THIS IS THE LANDING PAGE</div>
-        <div className="card-container">
-          <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
-          <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
-          <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
-          <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
-        </div>
-        */}
-  </div>
+      <LandingPage/>
+    </div>
   );
 };
 

eb2c8a12a7f13f5885d62cbb1b9fc2e1780a239d
svolterra <sarahkalmm1902@gmail.com>
2022-07-18 08:12:49 -0700

    Re-implement mission statement due to branching issues.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "@components/Card";
 import "./Home.css"
+import { TEXT } from '@statics';
 
 interface HomeProps {}
 
@@ -14,6 +15,18 @@ const Home: React.FC<HomeProps> = (props) => {
         <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
         <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
       </div>
+      <div className="mission_statement_title">
+          <p>{TEXT.LANDING_PAGE.MISSION_STATEMENT.TITLE}</p>
+          <hr className="horizontal_divider"/>
+        </div>
+        <div>
+          <p className="mission_statement">{TEXT.LANDING_PAGE.MISSION_STATEMENT.LAB_GOALS}</p>
+          <p className="mission_statement">{TEXT.LANDING_PAGE.MISSION_STATEMENT.LAB_GOALS}</p>
+        </div>
+        <div className="mission_img_container">
+          <div className="mission_img_placeholder"></div>
+          <div className="mission_img_placeholder"></div>
+        </div>
     </div>
   );
 };

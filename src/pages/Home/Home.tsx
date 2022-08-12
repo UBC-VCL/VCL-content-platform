8fa0d70d10aac9615824b0a87e65f265e3f5b3a9
svolterra <sarahkalmm1902@gmail.com>
2022-08-12 14:35:00 -0700

    Address code review comments and reformat layout.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,18 +15,20 @@ const Home: React.FC<HomeProps> = (props) => {
         <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
         <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
       </div>
-      <div className="mission-statement-title">
-          <p>{TEXT.LANDING_PAGE.MISSION_STATEMENT.TITLE}</p>
-          <hr className="horizontal-divider"/>
+      <div className="mission-statement-container">
+        <div className="mission-statement-title">
+            <p>{TEXT.LANDING_PAGE.MISSION_STATEMENT.TITLE}</p>
+            <hr className="horizontal-divider"/>
         </div>
         <div>
           <p className="mission-statement">{TEXT.LANDING_PAGE.MISSION_STATEMENT.LAB_GOALS}</p>
           <p className="mission-statement">{TEXT.LANDING_PAGE.MISSION_STATEMENT.LAB_GOALS}</p>
         </div>
-        <div className="mission-img-container">
-          <div className="mission-img-placeholder"></div>
-          <div className="mission-img-placeholder"></div>
-        </div>
+      </div>
+      <div className="mission-img-container">
+        <div className="mission-img-placeholder"></div>
+        <div className="mission-img-placeholder"></div>
+      </div>
     </div>
   );
 };

5bb132b1e70a0b98452665d43d2846bc7b3cd719
Taosit <taosit099@gmail.com>
2023-01-24 23:02:33 -0800

    deleted .next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/timeline/[id]/edit":["static\u002Fchunks\u002Fpages\u002Ftimeline\u002F[id]\u002Fedit.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ftimeline\u002F[id]\u002Fedit"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

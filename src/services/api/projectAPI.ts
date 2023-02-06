8c3eb48584d8bd670cc716512889723ad58b2821
Taosit <taosit099@gmail.com>
2023-02-05 22:42:11 -0800

    removed logs and fixed axios errors

--- src/services/api/projectAPI.ts
+++ src/services/api/projectAPI.ts
@@ -1,17 +1,9 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
-
-console.log("url", baseURL);
-
 export default class ProjectAPI {
-  // TODO: prefetching
   static async getAllProjects(): Promise<any> {
     try {
-      console.log("fetching")
-      const response = await axios.get("/api/projects", {
-        baseURL,
-      });
+      const response = await axios.get("/api/projects");
       return response;
     } catch (e) {
       throw e;
@@ -21,9 +13,7 @@ export default class ProjectAPI {
   static async getProjectByName(name: string): Promise<any> {
     // TODO: prefetching
     try {
-      const response = await axios.get(`/api/projects/${name}`, {
-        baseURL: "http://localhost:4000",
-      });
+      const response = await axios.get(`/api/projects/${name}`);
       return response;
     } catch (e) {
       throw e;
--- utils/helpers/server/connect.ts
+++ utils/helpers/server/connect.ts
@@ -8,14 +8,11 @@ global.mongo = global.mongo || {}
 
 export const connectToDB = async () => {
   if (!global.mongo.client) {
-    console.log("no mongo client")
     if (!process.env.DATABASE_URI) {
-      console.log("no database uri")
       throw new Error('No database URI')
     }
     global.mongo.client = new MongoClient(process.env.DATABASE_URI)
 
-    console.log('connecting to DB')
     await global.mongo.client.connect()
     console.log('connected to DB')
   }

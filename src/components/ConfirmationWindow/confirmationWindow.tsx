29c5812ea654e85dae7ca46d866bf2e9144a7a65
wyc <545301030@qq.com>
2023-06-16 16:07:12 +0800

    add some comment for this change

--- src/components/ConfirmationWindow/confirmationWindow.tsx
+++ src/components/ConfirmationWindow/confirmationWindow.tsx
@@ -14,6 +14,8 @@ export interface SimpleDialogProps {
 
 function ConfirmationDailog(props: SimpleDialogProps) {
     const { onClose, open, deleteSnapshot } = props;
+    // This state variable indicates if the snapshot selected is deleted successfully, which can be used to controll the ending of the dialog associated and error message.
+    // if the snapshot clicked is deleted,deleteSucces = true.
     const [deleteSuccess, setDeleteSuccess] = React.useState(true);
     const handleClose = () => {
       onClose();

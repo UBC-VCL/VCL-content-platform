0a31e399324e745f02101e5060fd5e76b8d8ea3e
Sara Ibrahim <71746168+svolterra@users.noreply.github.com>
2023-07-03 19:55:07 -0700

    Adjust website navbar bottom shadow (#89)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -137,7 +137,6 @@ const Navbar: React.FC<{}> = () => {
   return (
     <div className="nav">
       <div className="navbar-menu">
-        <AppBar position="sticky" className="nav-appbar">
         <Toolbar className="nav-toolbar">
           <div className="logo-container">
             <a href={ROUTES.HOME}>
@@ -173,7 +172,6 @@ const Navbar: React.FC<{}> = () => {
             )}
           </Menu>
         </Toolbar>
-      </AppBar>
       </div>
         <MobileMenu/>
     </div>

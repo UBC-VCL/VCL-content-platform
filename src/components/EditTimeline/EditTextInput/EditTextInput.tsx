5cb1df9b1b105f7ac87628210f35288564f73f0d
Erick Jovan Muljadi <ejovan2004@gmail.com>
2023-07-20 05:48:37 +0800

    get member id

--- src/components/EditTimeline/EditTextInput/EditTextInput.tsx
+++ src/components/EditTimeline/EditTextInput/EditTextInput.tsx
@@ -1,15 +1,53 @@
 import { TimelineInfo } from '@pages/Timeline/EditTimelineEntry/EditTimelineEntry';
-import React, { useEffect, useState } from 'react'
-import styles from './EditTextInput.module.css'
+import React, { useEffect, useState } from 'react';
+import styles from './EditTextInput.module.css';
+import axios from 'axios';
+import { useAppSelector, useAppDispatch } from '@redux/hooks';
+import { selectAuth } from '@redux/slices/AuthRedux';
+import { appActions } from '@redux/slices/AppRedux';
+import { FormControl, MenuItem, InputLabel, OutlinedInput } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 type Props = {
   timeline: TimelineInfo,
   setTimeline: React.Dispatch<React.SetStateAction<TimelineInfo>>,
 }
 
+interface memberProps {
+  isActive: boolean;
+  firstName: string;
+  lastName: string;
+  _id: string;
+}
+
 const EditTextInput = ({timeline, setTimeline}: Props) => {
+  const [contributors, setContributors] = React.useState<string[]>([]);
   const [isValid, setIsValid] = useState({author: true, title: true});
-  
+  const { access_token } = useAppSelector(selectAuth);
+  const [members, setMembers] = useState<memberProps[]>([]);
+  const dispatch = useAppDispatch();
+
+  async function fetchData() {
+    try { await axios({
+      method: "get",
+      url: "http://localhost:4000/api/members",
+      headers: {
+        authorization: access_token
+      }
+    })
+    .then((response) => {
+      setMembers([...response.data.data]);
+    })
+    .catch(err => {
+      dispatch(appActions.setAlert(err.message)); 
+    })
+    } catch (err) {
+      console.log("error");
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
   const checkValidity = (e: React.FocusEvent<HTMLInputElement>) => {
     const {name, value} = e.target;
     if (value === "") {
@@ -17,31 +55,62 @@ const EditTextInput = ({timeline, setTimeline}: Props) => {
     }
   }
 
+  const handleChangeAuthor = (event: SelectChangeEvent) => {
+    const { value } = event.target;
+    setTimeline(prev => ({...prev, author: value}));
+  }
+
+  const handleChangeContributors = (event: SelectChangeEvent) => {
+    const { value } = event.target;
+    setTimeline(prev => ({...prev, contributors: value.join(', ')}));
+    const currentContributor = typeof value === 'string' ? value.split(',') : value;
+    setContributors(currentContributor);
+  }
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {name, value} = e.target;
     setIsValid(prev => ({...prev, [name]: true}));
     setTimeline(prev => ({...prev, [name]: value}));
   }
-
+  console.log(timeline);
   return (
     <div className={styles.textInputs}>
-      <div className={styles.textInputGroup}>
-        <input type="text" name="author" value={timeline.author} placeholder="Author"
-        className={isValid.author ? styles.textInput : `${styles.textInput} ${styles.textInputInvalid}`}
-        onChange={handleChange} onBlur={checkValidity}/>
-        <p className={styles.hintText}>e.g. John Doe</p>
-      </div>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Author</InputLabel>
+        <Select
+          value={timeline.author}
+          label="Author"
+          onChange={handleChangeAuthor}
+        >
+          {members.map((member, i) => (
+            <MenuItem key={i} value={member._id}>
+              {member.firstName + " " + member.lastName}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <div className={styles.textInputGroup}>
         <input type="text" name="title" value={timeline.title} placeholder="Title" 
         className={isValid.title ? styles.textInput : `${styles.textInput} ${styles.textInputInvalid}`}
         onChange={handleChange} onBlur={checkValidity} />
         <p className={styles.hintText}>e.g. Documentation Website Update</p>
       </div>
-      <div className={styles.textInputGroup}>
-        <input type="text" name="contributors" value={timeline.contributors} placeholder="Contributors" 
-        className={styles.textInput} onChange={handleChange} />
-        <p className={styles.hintText}>e.g. James Doe, Janet Doe</p>
-      </div>
+      <FormControl fullWidth>
+        <InputLabel>Contributors</InputLabel>
+      <Select
+          maxRows={5}
+          multiple
+          value={contributors}
+          onChange={handleChangeContributors}
+          input={<OutlinedInput label="Contributors" />}
+        >
+          {members.map((member, i) => (
+            <MenuItem key={i} value={member._id}>
+              {member.firstName + " " + member.lastName}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </div>
   )
 }

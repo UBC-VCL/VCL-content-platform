1aa78ab7efd22ca606b46752c6c7d1d44e904d9d
Ryan <ryancyae@gmail.com>
2023-07-18 21:07:00 -0700

    started ticket
    made div for inputs

--- src/pages/Project/ProjectTeam.tsx
+++ src/pages/Project/ProjectTeam.tsx
@@ -5,41 +5,59 @@ import { Person } from '../../components/ProjectPeople/ProjectPeople';
 import { TEXT } from '@statics'
 import "./ProjectTeam.css";
 import "./Project.css";
+import { useState } from 'react'
 
 interface ProjectProps {
-    project : Project,
+    project: Project,
 }
 
 const ProjectTeam: React.FC<ProjectProps> = (props) => {
+
+    const [viewState, setViewState] = useState<boolean>(false)
+
     return (
         <div className='project-subcontent-container'>
-        <div className="project-team">
-            <div>
-                <ProjectBreadcrumbs project_name={props.project.name} page_name={TEXT.PROJECT_NAV.TEAM_AND_ALUMNI} /> 
+            
+            <div className='add-viewing-div' style={{ display: viewState ? 'block' : 'none' }}>
+            
             </div>
-            <div className="team-header">Team Members</div>
-            <hr/>
-            <div>
-            <Person 
-                name="Kevin Peng"
-                involvement='Project Lead, Graduate Student, Coding Team Manager, Analytics Manager'
-                description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?'
-                email='mellio10@psych.ubc.ca'
-                phone='604 000 0000'
-                linkedIn='linkedin.com'
-                isCurrentMember={true}
-            />
-            <Person 
-            name="Maddison Eliot"
-            involvement='Project Lead, Graduate Student, Coding Team Manager, Analytics Manager'
-            description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?'
-            email='mellio10@psych.ubc.ca'
-            phone='604 000 0000'
-            linkedIn='linkedin.com'
-            isCurrentMember={false}
-        />
-        </div>
-        </div>
+            <div className="project-team" style={{ pointerEvents: viewState ? 'none' : 'auto'}}>
+                <div>
+                    <ProjectBreadcrumbs project_name={props.project.name} page_name={TEXT.PROJECT_NAV.TEAM_AND_ALUMNI} />
+                </div>
+                <div className="team-header">
+                    <p>
+                        Team Members
+                    </p>
+                </div>
+                <hr />
+                <div className='add-mem-button-div'>
+                    <div onClick={() => setViewState(!viewState)}>
+                        + Add Member
+                    </div>
+                </div>
+                <div>
+                    <Person
+                        name="Kevin Peng"
+                        involvement='Project Lead, Graduate Student, Coding Team Manager, Analytics Manager'
+                        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?'
+                        email='mellio10@psych.ubc.ca'
+                        phone='604 000 0000'
+                        linkedIn='linkedin.com'
+                        isCurrentMember={true}
+                    />
+                    <Person
+                        name="Maddison Eliot"
+                        involvement='Project Lead, Graduate Student, Coding Team Manager, Analytics Manager'
+                        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?'
+                        email='mellio10@psych.ubc.ca'
+                        phone='604 000 0000'
+                        linkedIn='linkedin.com'
+                        isCurrentMember={false}
+                    />
+                </div>
+            </div>
+
         </div>
     )
 };

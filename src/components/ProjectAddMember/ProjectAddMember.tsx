bd87537b6e123808130f60398b00191d34d5eb45
Ryan <ryancyae@gmail.com>
2023-08-01 21:09:14 -0700

    updated inputs

--- src/components/ProjectAddMember/ProjectAddMember.tsx
+++ src/components/ProjectAddMember/ProjectAddMember.tsx
@@ -1,5 +1,8 @@
+import { color } from '@mui/system';
 import './ProjectAddMember.css'
 import { GrClose } from "react-icons/gr";
+import { useState } from 'react';
+
 
 interface PropsOBJ {
     isVisible: boolean;
@@ -10,6 +13,19 @@ const ProjectAddMember = (props: PropsOBJ) => {
 
     const { isVisible, setVisibility } = props;
 
+    const [inputtedProjects, setProjects] = useState<Array<string>>([])
+    const [currentPInput, setPInput] = useState<string>("")
+
+    const handleSubmit = () => {
+        
+    }
+
+    const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (event) => {
+        if (event.key == 'Enter') {
+            console.log('pressed enter for a project')
+        }
+    }
+
     return (
         <>
             <div className='add-viewing-div' style={{ display: isVisible ? 'block' : 'none' }}>
@@ -17,19 +33,34 @@ const ProjectAddMember = (props: PropsOBJ) => {
                 <div className='content-viewing-div'>
                     <div className='inputs-div'>
                         <div className='input-div'>
-                            <input type='text' id='name-input' className='input' placeholder="Full name" />
-                            <input type='text' id='involvement-input' className='input' placeholder="involvement" />
+                            <input type='text' id='firstname-input' className='input' placeholder=" First name" />
+                            <input type='text' id='lastname-input' className='input' placeholder=" Last name" />
+                            <input type='text' id='project-input' className='input' placeholder=" Projects" 
+                            onChange={(e) => setPInput(e.target.value)}
+                            onKeyDown={handleKeyDown}
+                            />
+                            {/* <input type='text' id='involvement-input' className='input' placeholder="involvement" />
                             <input type='text' id='email-input' className='input' placeholder='email123@email.com' />
                             <input type='text' id='phone-input' className='input' placeholder="123-45-678" />
-                            <input type='text' id='linkedInInput' className='input' placeholder="LinkedIn" />
+                            <input type='text' id='linkedInInput' className='input' placeholder="LinkedIn" /> */}
                         </div>
                         <div className='display-involvement'>
-
+                            {
+                                inputtedProjects.map(
+                                    (item) => {
+                                        return (
+                                            <div>
+                                                {item}
+                                            </div>
+                                        )
+                                    }
+                                )
+                            }
                         </div>
                     </div>
                     <div className='button-div'>
-                        <div className='button'>
-                            <p>
+                        <div className='button' onClick={handleSubmit}>
+                            <p style={{color:"white"}}>
                                 Submit
                             </p>
                         </div>

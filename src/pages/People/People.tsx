d136bb4ab130625077f95ff06c7071602347f019
Ryan <ryancyae@gmail.com>
2023-08-21 21:01:41 -0700

    Finished navbar functionality

--- src/pages/People/People.tsx
+++ src/pages/People/People.tsx
@@ -1,17 +1,29 @@
 import react from 'react';
+import { useState } from 'react'
 import './People.css'
 
 const People = () => {
+
+    const dummyList = ['Supervisors', 'Correlation', 'IDEO', 'IT', 'NOVA', 'Perceptiual Modes']
 
     //TODO:
     //  you will dynamically render all the members for each of the respective projects when the
     //  user is to swtich between projects.
     //      - Make a http request everytime the desired project changes
 
+    // TODO: 
+    //  Given the grid items, when the user is to click on them the page will filter automatically
+    //      - The clicked on item should be highlighted
+
+
+    // the page will be defaulted to bein on the first grid item
+    const [currentProject, setCProject] = useState<string>("Supervisors")
+
 
     return (
         <>
             <div className='main-content'>
+
                 <div className='project-header'>
                     <h1 className='page-title'>
                         OUR TEAM
@@ -20,37 +32,29 @@ const People = () => {
                         Meet the members of our lab and their respective teams
                     </h4>
                 </div>
+                {/*
+                    The items within the grid should be done dynamically, it should have inline styling for the number of items, instead of it being inside the css 
+                     - An issue that comes with this is the that the nav bar width will become too wide and some titles will not fit within each grid component 
+                */}
                 <div className='page-nav'>
-                    <div className='nav-item'>
-                        <div className='hover-item'>
-                            Supervisors
-                        </div>
-                    </div>
-                    <div className='nav-item'>
-                        <div className='hover-item'>
-                            Correlation
-                        </div>
-                    </div>
-                    <div className='nav-item'>
-                        <div className='hover-item'>
-                            IDEO
-                        </div>
-                    </div>
-                    <div className='nav-item'>
-                        <div className='hover-item'>
-                            IT
-                        </div>
-                    </div>
-                    <div className='nav-item'>
-                        <div className='hover-item'>
-                            NOVA
-                        </div>
-                    </div>
-                    <div className='nav-item'>
-                        <div className='hover-item'>
-                            Perceptual Modes
-                        </div>
-                    </div>
+                    {
+                        dummyList.map((item: string, index: number) => {
+                            return (
+                                <div key={index} className='nav-item'
+                                    onClick={() => {
+                                        // remove the 'selected-items' className before setting a new currentProject
+                                        document.getElementById(currentProject.toLowerCase())!.classList.remove('selected-item')
+                                        setCProject(item)
+                                        document.getElementById(item.toLowerCase())!.classList.add('selected-item')
+                                    }}
+                                >
+                                    <div className='hover-item' id={item.toLowerCase()}>
+                                        {item}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
                 <div className='content-display'>
 

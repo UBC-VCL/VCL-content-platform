0f5a6ebfba4ba52019b1b1f2901d0c94b9671d51
ryanyae <ryancyae@gmail.com>
2023-09-13 11:17:10 -0700

    Fixed Comments

--- src/statics/nav.ts
+++ src/statics/nav.ts
@@ -5,34 +5,6 @@ require('dotenv').config();
 
 const IS_WIP = process.env.REACT_APP_WIP;
 
-// const NAV = [
-// 	{
-// 		TITLE: TEXT.PAGE_TITLES.HOME,
-// 		REF: ROUTES.HOME
-// 	},
-// 	{
-// 		TITLE: TEXT.PAGE_TITLES.PROJECTS,
-// 		REF: ROUTES.PROJECT.BASE
-// 	},
-// 	{
-// 		TITLE: TEXT.PAGE_TITLES.PEOPLE,
-// 		REF: ROUTES.PEOPLE
-// 	},
-// 	{
-// 		TITLE: TEXT.PAGE_TITLES.GET_INVOLVED,
-// 		REF: ROUTES.GET_INVOLVED
-// 	},
-// 	{
-// 		TITLE: TEXT.PAGE_TITLES.TIMELINE,
-// 		REF: ROUTES.TIMELINE
-// 	},
-// 	{
-// 		TITLE: TEXT.PAGE_TITLES.RESOURCES,
-// 		REF: ROUTES.RESOURCES
-// 	}
-// ];
-
-
 const baseNav = [
 	{
 	  TITLE: TEXT.PAGE_TITLES.HOME,

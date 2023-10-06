a930aaeb04387d1b8cd05092d988ee11175dd9d6
ryanyae <ryancyae@gmail.com>
2023-10-06 12:13:46 -0700

    Update People.tsx

--- src/pages/People/People.tsx
+++ src/pages/People/People.tsx
@@ -8,10 +8,8 @@ import { AiFillLinkedin, AiFillPhone } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
 import { MdAccountCircle } from "react-icons/md";
 import Alert from "@mui/material/Alert";
-import TEXT from '@statics/text';
-import { CircularProgress} from '@mui/material';
-
-
+import TEXT from "@statics/text";
+import { CircularProgress } from "@mui/material";
 
 dotenv.config();
 const baseURL = process.env.REACT_APP_API_URL;
@@ -50,22 +48,21 @@ const People = () => {
 
   // This is for styles, will highlight the first select nav-item for the user
   useEffect(() => {
-    loadingTimer(2000)
+    loadingTimer(2000);
     getMembers();
     document
       .getElementById(dummyList[0].toLowerCase())!
       .classList.add("selected-item");
   }, []);
 
-
   // timer in milliseconds, sets how long the loading icon should be shown.
   // for now it is hardcoded timer because the calls that we make on this page is very fast so
   //    setting the state of svgView in the API call will only show the icon for not even 1 second.
-  const loadingTimer = (timer:number) => {
+  const loadingTimer = (timer: number) => {
     setTimeout(() => {
-        setSvgView(false);
-      }, timer);
-  }
+      setSvgView(false);
+    }, timer);
+  };
 
   const getMembers = async () => {
     axios
@@ -119,12 +116,15 @@ const People = () => {
         </div>
         <div className="content-display">
           <div className="member-list">
-            {
-                svgView ?
-                <CircularProgress></CircularProgress> : (
-                    resSuccess ? 
-            (
-              currentList.filter((item) => {return (item.position.toLowerCase() === currentProject.toLowerCase() ||item.project.toLowerCase() === currentProject.toLowerCase());
+            {svgView ? (
+              <CircularProgress></CircularProgress>
+            ) : resSuccess ? (
+              currentList.filter((item) => {
+                return (
+                  item.position.toLowerCase() ===
+                    currentProject.toLowerCase() ||
+                  item.project.toLowerCase() === currentProject.toLowerCase()
+                );
               }).length > 0 ? (
                 currentList
                   .filter((item) => {
@@ -162,20 +162,16 @@ const People = () => {
                       </div>
                     );
                   })
-              ) 
-            : (
+              ) : (
                 <Alert severity="info" className="people-page-prompt-string">
                   {TEXT.PEOPLE_PAGE.EMPTY_DISPLAY_LIST}
                 </Alert>
               )
-            ) 
-            : (
+            ) : (
               <Alert severity="error" className="people-page-prompt-string">
-                    {TEXT.PEOPLE_PAGE.RESPONSE_ERROR}
+                {TEXT.PEOPLE_PAGE.RESPONSE_ERROR}
               </Alert>
-            )
-                )
-            }
+            )}
           </div>
         </div>
       </div>

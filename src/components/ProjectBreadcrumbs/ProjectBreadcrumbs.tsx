5e2d4bd4afc6592deb028ae0471c89b8f7802119
Ketan Saraf <saraf.ketan1108@gmail.com>
2023-10-24 13:33:00 -0700

    fix: menu icon & breadcrumbs

--- src/components/ProjectBreadcrumbs/ProjectBreadcrumbs.tsx
+++ src/components/ProjectBreadcrumbs/ProjectBreadcrumbs.tsx
@@ -1,36 +1,54 @@
-import React from 'react';
-import './ProjectBreadcrumbs.css';
-import Breadcrumbs from '@mui/material/Breadcrumbs'
-import Link from '@mui/material/Link'
-import Typography from '@mui/material/Typography'
-import ROUTES from '@statics/routes'
-
+import React from "react";
+import "./ProjectBreadcrumbs.css";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import ROUTES from "@statics/routes";
+import { useLocation } from "react-router-dom";
 interface ProjectBreadcrumbsParam {
-    project_name: string,
-    page_name: string
+  project_name: string;
+  page_name: string;
 }
 
 const ProjectBreadcrumbs: React.FC<ProjectBreadcrumbsParam> = (props) => {
-    return (
-        <Breadcrumbs 
-            separator=">" 
-            style={{ 
-                marginTop: "4%",
-                marginBottom: "6%",
-                color: "#AEC7EC",
-                marginLeft:"-3%"
-            }}>
-            <Link href={ROUTES.PROJECT.BASE} underline="none" id="project-breadcrumbs">
-                 All Projects 
-            </Link>
-            <Link href={ROUTES.PROJECT.BASE + '/' + props.project_name} underline="none" id="project-breadcrumbs"> 
-                {props.project_name} 
-            </Link>
-            <Typography className="current-page">
-                {props.page_name} 
-            </Typography>
-        </Breadcrumbs>
-    )    
-}
+  const { pathname } = useLocation();
+  const lightModePages = ["join", "resources", "team", "publications"];
+  const isLightMode = lightModePages.includes(pathname.split("/")[3]);
+
+  return (
+    <Breadcrumbs
+      separator=">"
+      style={{
+        marginTop: "4%",
+        marginBottom: "6%",
+        color: "#AEC7EC",
+        marginLeft: "-3%",
+      }}
+    >
+      <Link
+        href={ROUTES.PROJECT.BASE}
+        underline="none"
+        id="project-breadcrumbs"
+        style={{ color: isLightMode ? "#AEC7EC" : "#60779A" }}
+      >
+        <b>All Projects</b>
+      </Link>
+      <Link
+        href={ROUTES.PROJECT.BASE + "/" + props.project_name}
+        underline="none"
+        id="project-breadcrumbs"
+        style={{ color: isLightMode ? "#AEC7EC" : "#60779A" }}
+      >
+        <b>{props.project_name} </b>
+      </Link>
+      <Typography
+        className="current-page"
+        style={{ color: isLightMode ? "#60779A" : "#AEC7EC" }}
+      >
+        <b>{props.page_name}</b>
+      </Typography>
+    </Breadcrumbs>
+  );
+};
 
 export default ProjectBreadcrumbs;

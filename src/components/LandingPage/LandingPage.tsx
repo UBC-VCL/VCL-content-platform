c9c6629b1d248fe9c1349a860a22635f750da75f
ryanyae <ryancyae@gmail.com>
2023-11-21 14:42:01 -0800

    Update LandingPage.tsx

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -9,19 +9,6 @@ const LandingPage = () => {
       <div id="info-section">
         <div id="title">{TEXT.LANDING_PAGE.TITLE}</div>
         <div id="lab-description">{TEXT.LANDING_PAGE.DESCRIPTION}</div>
-        <div className="btn-ubc-home">
-          <a
-            href="https://www.ubc.ca"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              color: "#ffffff",
-            }}
-          >
-            <p>{TEXT.LANDING_PAGE.UBC_PAGE_BUTTON}</p>
-          </a>
-        </div>
       </div>
       <div id="landing-page-img-container">
         <img src={VCLIcon} className="landing-page-logo"></img>

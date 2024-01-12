479a2464d10d669996d6c147f7866f18a1aa007f
Ryan Yae <ryancyae@gmail.com>
2024-01-11 17:12:10 -0800

    Added visibility change to the addResource tab

--- src/pages/Resources/Page/AddResource/AddResource.tsx
+++ src/pages/Resources/Page/AddResource/AddResource.tsx
@@ -1,14 +1,19 @@
 import './AddResource.css';
+import { IoCloseSharp } from "react-icons/io5";
 
-const AddResource = () => {
+const AddResource: React.FC<{ isVisible: boolean, setVisible:(bool:boolean)=>void }> = ({ isVisible, setVisible }) => {
 
     return (
-        <div className='add-resource-content-container'>
+        <div className='add-resource-content-container' style={{display:`${isVisible ? '' : 'none'}`}}>
             <div className='add-resource-content'>
                 <div className='add-resource-close-tab-container'>
-                    <div className='add-resource-close-tab-button'>
+                    {/* <div >
                         close
-                    </div>
+                    </div> */}
+                    <IoCloseSharp className='add-resource-close-tab-button' size={45} 
+                    onClick={() => {
+                        setVisible(false);
+                    }}/>
                 </div>
                 <div className='add-resource-input-containers'>
                     <div className='add-resource-input add-resource-input-containers-child' id='add-resource-title-input-container'>
@@ -19,14 +24,11 @@ const AddResource = () => {
                         <h2>Description</h2>
                         <textarea/>
                     </div>
+                    <div className='add-resource-input-containers-child'>
+                        <input type='file'/>
+                    </div>
                     <div className='add-resource-input add-resource-input-containers-child' id='add-resource-file-input-container'>
                         <h2>Uploaded</h2>
-                    </div>
-                    <div className='add-resource-input-containers-child'>
-                        {
-                            // where you render which files have ben uploaded
-                            "uploaded files"
-                        }
                     </div>
                     <div id='add-resource-submit-button' className='add-resource-input-containers-child'>
                         <h2>
--- src/pages/Resources/Page/ResourcePage.tsx
+++ src/pages/Resources/Page/ResourcePage.tsx
@@ -17,15 +17,17 @@ const ResourcePage: React.FC<ProjectProps> = ({ match }) => {
 
     const currentResource = RESOURCES.CONTENT.find((resource) => resource.title === match.params.resource_id);
 
+
     const [navHeight, setNavHeight] = useState<number>(0);
     const [filterDates, setFilterDates] = useState<string[]>([currDate.getFullYear().toString(), (currDate.getFullYear() - 1).toString(), (currDate.getFullYear() - 2).toString()]);
-    const [currentFilter, setFilter] = useState<string>(currentResource!.headers ? currentResource!.headers[0] : filterDates[0]);
+    // const [currentFilter, setFilter] = useState<string>(currentResource!.headers ? currentResource!.headers[0] : filterDates[0]);
+    const [isAddMenuVisible, setAddMenuVisible] = useState<boolean>(false);
 
-    useEffect(() => {
-        document
-            .getElementById(currentFilter)!
-            .classList.add("selected-item");
-    }, [])
+    // useEffect(() => {
+    //     document
+    //         .getElementById(currentFilter)!
+    //         .classList.add("selected-item");
+    // }, [])
 
     useEffect(() => {
         // error handling for pages that do not exist yet
@@ -46,13 +48,15 @@ const ResourcePage: React.FC<ProjectProps> = ({ match }) => {
 
     return (
         <>
-            <div className='resource-page-container' style={{ marginTop: navHeight }}>
-                <div className='resource-page-title-container'>
-                    <h1 className='resource-page-title'>
-                        {currentResource?.page_title}
-                    </h1>
-                </div>
-                <div className='resource-content-containers' style={{ gridTemplateColumns: `repeat(${currentResource!.headers ? currentResource!.headers.length : 3}, 1fr)` }}>
+            <AddResource isVisible={isAddMenuVisible} setVisible={setAddMenuVisible} />
+            <div style={{ filter: isAddMenuVisible ? 'blur(10px)' : 'blur(0px)', transition: 'all 0.5s ease-in-out', }}>
+                <div className='resource-page-container' style={{ marginTop: navHeight }}>
+                    <div className='resource-page-title-container'>
+                        <h1 className='resource-page-title'>
+                            {currentResource?.page_title}
+                        </h1>
+                    </div>
+                    {/* <div className='resource-content-containers' style={{ gridTemplateColumns: `repeat(${currentResource!.headers ? currentResource!.headers.length : 3}, 1fr)` }}>
                     {
                         currentResource!.headers ? currentResource!.headers.map((title, index) => {
 
@@ -97,72 +101,29 @@ const ResourcePage: React.FC<ProjectProps> = ({ match }) => {
                             );
                         })
                     }
+                </div> */}
                 </div>
-            </div>
-            <div className='resource-content-render-container'>
-                {
-                    currentResource!.headers ?
-                        currentResource!.headers.map((title, index) => {
-                            return (
-                                <>
-                                    <div className='resource-content-single-render-category' key={index}>
-                                        <h1 className='resource-content-single-render-category-title'>
-                                            {title}
-                                        </h1>
-                                    </div>
-                                    <div className='resource-content-single-render-category-content'>
-                                        {
-                                            currentResource!.content.filter((item) => {
-                                                return item.category == title;
-                                            }).map((item, index) => {
-                                                return (
-                                                    <div className='resource-content-single-render' key={index}>
-                                                        <div className='resource-content-single-render-image-container'>
-                                                            <img src={item.img} alt='NO IMAGE'></img>
-                                                        </div>
-                                                        <div className='resource-content-single-render-text-container'>
-                                                            <h1>
-                                                                {item.title}
-                                                            </h1>
-                                                            <div className='resource-content-single-render-text-names-container'>
-                                                                {
-                                                                    item.author.map((name, index) => {
-                                                                        return index !== item.author.length - 1 ? <h2>{name}, &nbsp;</h2> : <h2>{name}</h2>
-                                                                    })
-                                                                }
-                                                            </div>
-                                                            <p>
-                                                                {item.intro}
-                                                            </p>
-                                                        </div>
-                                                    </div>
-                                                )
-                                            })
-                                        }
-                                    </div>
-                                </>
-                            )
-                        }) :
-
-                        <>
-                            {filterDates.map((date, index) => {
+                <div className='resource-content-render-container'>
+                    {
+                        currentResource!.headers ?
+                            currentResource!.headers.map((title, index) => {
                                 return (
                                     <>
                                         <div className='resource-content-single-render-category' key={index}>
-                                            <h1 className='resource-content-single-render-category-title'>
-                                                {date}
+                                            <h1 className='resource-content-single-render-category-title'
+                                            >
+                                                {title}
                                             </h1>
                                         </div>
                                         <div className='resource-content-single-render-category-content'>
                                             {
                                                 currentResource!.content.filter((item) => {
-                                                    const year: number = (item.category as Date).getFullYear();
-                                                    return year === Number(date);
+                                                    return item.category == title;
                                                 }).map((item, index) => {
                                                     return (
                                                         <div className='resource-content-single-render' key={index}>
                                                             <div className='resource-content-single-render-image-container'>
-                                                                <img src={item.img} alt='NO IMAGE'></img>
+                                                                <img src={item.img} alt='NO IMAGE' id="resource-content-single-render-image"></img>
                                                             </div>
                                                             <div className='resource-content-single-render-text-container'>
                                                                 <h1>
@@ -186,49 +147,100 @@ const ResourcePage: React.FC<ProjectProps> = ({ match }) => {
                                         </div>
                                     </>
                                 )
-                            })}
-                            <div className='resource-content-single-render-category'>
-                                <h1 className='resource-content-single-render-category-title'>
-                                    Older Content
-                                </h1>
-                            </div>
-                            <div className='resource-content-single-render-category-content'>
-                                {
-                                    currentResource!.content.filter((item) => {
-                                        const year: number = (item.category as Date).getFullYear();
-                                        return year < Number(filterDates[2]);
-                                    }).map((item, index) => {
-                                        return (
-                                            <div className='resource-content-single-render' key={index}>
-                                                <div className='resource-content-single-render-image-container'>
-                                                    <img src={item.img} alt='NO IMAGE'></img>
-                                                </div>
-                                                <div className='resource-content-single-render-text-container'>
-                                                    <h1>
-                                                        {item.title}
-                                                    </h1>
-                                                    <div className='resource-content-single-render-text-names-container'>
-                                                        {
-                                                            item.author.map((name, index) => {
-                                                                return index !== item.author.length - 1 ? <h2>{name}, &nbsp;</h2> : <h2>{name}</h2>
-                                                            })
-                                                        }
-                                                    </div>
-                                                    <p>
-                                                        {item.intro}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
-                        </>
-                }
-            </div>
-            <AddResource />
-            <div className='add-resource-button-container'>
+                            }) :
 
+                            <>
+                                {filterDates.map((date, index) => {
+                                    return (
+                                        <>
+                                            <div className='resource-content-single-render-category' key={index}>
+                                                <h1 className='resource-content-single-render-category-title'>
+                                                    {date}
+                                                </h1>
+                                            </div>
+                                            <div className='resource-content-single-render-category-content'>
+                                                {
+                                                    currentResource!.content.filter((item) => {
+                                                        const year: number = (item.category as Date).getFullYear();
+                                                        return year === Number(date);
+                                                    }).map((item, index) => {
+                                                        return (
+                                                            <div className='resource-content-single-render' key={index}>
+                                                                <div className='resource-content-single-render-image-container'>
+                                                                    <img src={item.img} alt='NO IMAGE'></img>
+                                                                </div>
+                                                                <div className='resource-content-single-render-text-container'>
+                                                                    <h1>
+                                                                        {item.title}
+                                                                    </h1>
+                                                                    <div className='resource-content-single-render-text-names-container'>
+                                                                        {
+                                                                            item.author.map((name, index) => {
+                                                                                return index !== item.author.length - 1 ? <h2>{name}, &nbsp;</h2> : <h2>{name}</h2>
+                                                                            })
+                                                                        }
+                                                                    </div>
+                                                                    <p>
+                                                                        {item.intro}
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
+                                        </>
+                                    )
+                                })}
+                                <div className='resource-content-single-render-category'>
+                                    <h1 className='resource-content-single-render-category-title'>
+                                        Older Content
+                                    </h1>
+                                </div>
+                                <div className='resource-content-single-render-category-content'>
+                                    {
+                                        currentResource!.content.filter((item) => {
+                                            const year: number = (item.category as Date).getFullYear();
+                                            return year < Number(filterDates[2]);
+                                        }).map((item, index) => {
+                                            return (
+                                                <div className='resource-content-single-render' key={index}>
+                                                    <div className='resource-content-single-render-image-container'>
+                                                        <img src={item.img} alt='NO IMAGE'></img>
+                                                    </div>
+                                                    <div className='resource-content-single-render-text-container'>
+                                                        <h1>
+                                                            {item.title}
+                                                        </h1>
+                                                        <div className='resource-content-single-render-text-names-container'>
+                                                            {
+                                                                item.author.map((name, index) => {
+                                                                    return index !== item.author.length - 1 ? <h2>{name}, &nbsp;</h2> : <h2>{name}</h2>
+                                                                })
+                                                            }
+                                                        </div>
+                                                        <p>
+                                                            {item.intro}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </>
+                    }
+                </div>
+                <div className='add-resource-button-container'
+                    onClick={() => {
+                        window.scrollTo({
+                            top: 0,
+                            behavior: 'smooth'
+                        });
+                        setAddMenuVisible(true);
+                    }}>
+                    Add New Resource
+                </div>
             </div>
         </>
     );

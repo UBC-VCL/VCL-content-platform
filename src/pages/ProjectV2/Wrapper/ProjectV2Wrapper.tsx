a87798245409ac6396268f750f68157bf17758fe
Ryan Yae <ryancyae@gmail.com>
2024-02-05 10:57:02 -0800

    finished

--- src/pages/ProjectV2/Wrapper/ProjectV2Wrapper.tsx
+++ src/pages/ProjectV2/Wrapper/ProjectV2Wrapper.tsx
@@ -46,30 +46,41 @@ const ProjectV2Wrapper: React.FC<ProjectProps> = ({ match }) => {
         <div className="project-content-background"
         // style={{backgroundImage:`linearGradient(to right,${ white 50%},%{blue 50%})`}}
         >
-            <div className="project-content-container" style={{ paddingTop: `${menuSize}px` }}>
+            <div className="project-content-container">
                 <div id="projectV2-sidebar">
-                    <div className="exit-icon-container">
-                        <MdExitToApp size={30} />
-                    </div>
-                    <div style={{marginTop:"3rem"}}>
-                        <div className="single-table-content">
-                            <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/`}>Home</a>
-                        </div>
-                        <div className="single-table-content">
-                            <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/join`}>Join</a>
-                        </div>
-                        <div className="single-table-content">
-                            <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/team`}>Team</a>
-                        </div>
-                        <div className="single-table-content">
-                            <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/resources`}>Resources</a>
-                        </div>
-                        <div className="single-table-content">
-                            <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/publications`}>Publications</a>
+                    <div>
+                        {/* <div className="exit-icon-container">
+                            <MdExitToApp size={30} />
+                        </div> */}
+                        <div style={{ textAlign: "center", marginTop: "1rem" }}>
+                            <div className="project-sidebar-title">
+                                <h2>Now Viewing</h2>
+                                <h1>{match.params.project_id}</h1>
+                            </div>
+                            <div className="project-sidebar-clickable-content">
+                                <div className="single-table-content">
+                                    <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/`}>Home</a>
+                                </div>
+                                <div className="single-table-content">
+                                    <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/join`}>Join</a>
+                                </div>
+                                <div className="single-table-content">
+                                    <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/team`}>Team</a>
+                                </div>
+                                <div className="single-table-content">
+                                    <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/resources`}>Resources</a>
+                                </div>
+                                <div className="single-table-content">
+                                    <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/publications`}>Publications</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div id="table-contents">
+                <div id="table-contents" style={{
+                    paddingTop: // `${menuSize}px` 
+                        "8rem"
+                }}>
                     <div className="single-table-content">
                         <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/`}>Home</a>
                     </div>
@@ -86,7 +97,7 @@ const ProjectV2Wrapper: React.FC<ProjectProps> = ({ match }) => {
                         <a className="project-content-container-href" href={`/projectsV2/${match.params.project_id}/publications`}>Publications</a>
                     </div>
                 </div>
-                <div className='project-router-div'>
+                <div className='project-router-div' style={{ paddingTop: `${menuSize}px` }}>
                     <ProjectRouter project_id={match.params.project_id} />
                 </div>
             </div>
